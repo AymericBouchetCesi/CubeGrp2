6eaab6129cf9cdfd88e55bc979bc8a8a0df1d6f9
Aymeric Bouchet <aymeric.bouchet@viacesi.fr>
2021-03-07 22:48:17 +0100

    🚧 Header et Footer (en cours)

--- r2/src/vue/connection/Connection.tsx
+++ r2/src/vue/connection/Connection.tsx
@@ -1,13 +1,15 @@
 import {IonPage, IonContent} from "@ionic/react";
 import React from "react";
 import './Connection.css';
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Connection: React.FC = () => {
     return (
         <IonPage>
-            <IonContent color={"primary"}>
-                <h1>Connection</h1>
-            </IonContent>
+            <Header/>
+            <IonContent/>
+            <Footer/>
         </IonPage>
     );
 };
