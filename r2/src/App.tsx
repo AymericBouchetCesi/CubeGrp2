e3b5ca600073327070610510067301e415f3efbc
Aymeric Bouchet <aymeric.bouchet@viacesi.fr>
2021-03-07 18:08:18 +0100

    🎨 Restructuration des fichiers

--- r2/src/App.tsx
+++ r2/src/App.tsx
@@ -1,18 +1,10 @@
-import { Redirect, Route } from 'react-router-dom';
+import {Redirect, Route} from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+    IonApp,
+    IonRouterOutlet,
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import {IonReactRouter} from '@ionic/react-router';
+import Connection from "./vue/connection/Connection";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,42 +24,21 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import React from "react";
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonRouterOutlet>
+                <Route exact path="/connection">
+                    <Connection/>
+                </Route>
+                <Route exact path="/">
+                    <Redirect to="/connection"/>
+                </Route>
+            </IonRouterOutlet>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
---
+++ r2/src/vue/connection/Connection.tsx
@@ -0,0 +1,15 @@
+import {IonPage, IonContent} from "@ionic/react";
+import React from "react";
+import './Connection.css';
+
+const Connection: React.FC = () => {
+    return (
+        <IonPage>
+            <IonContent color={"primary"}>
+                <h1>Connection</h1>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Connection;
