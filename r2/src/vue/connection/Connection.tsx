fb45ade56908cb5e8b2305f9f663bcece8bafb15
Killian <Naillik.pc@live.fr>
2021-03-08 15:53:38 +0100

    Page fait

--- r2/src/vue/connection/Connection.tsx
+++ r2/src/vue/connection/Connection.tsx
@@ -1,21 +1,31 @@
 import {IonPage, IonContent, IonInput, IonButton, IonIcon, IonCard, IonCardTitle, IonCheckbox} from "@ionic/react";
 import React, { useState } from "react";
-import './Inscription.css';
+import './Connection.css';
 
 const Connection: React.FC = () => {
-    const [input,setInput] = useState<string>('')
+    const [userMail,setUserMail] = useState('')
+    const [password,setPassword] = useState('')
+    function login() {
+        console.log(userMail, password)
+    }
+
+
     return (
         <IonPage>
             <IonContent color={"light"}>
                 <IonCard color={"secondary"}>
                     <IonCardTitle>Connection</IonCardTitle>
-                    <IonInput value={input} type="email">Mail</IonInput> 
-                    <IonInput value={input} type="password">Mot de passe
-                    </IonInput>
-                    <IonCheckbox>
-                            <IonIcon name="eye-outline"></IonIcon>
-                    </IonCheckbox>
-                    <IonButton color ="primary">Connection </IonButton>
+                    <IonInput name="mail" value={userMail} type="email" 
+                    placeholder="VotreEmail@aaaaa.aa"
+                    onIonChange={(e: any) => setUserMail(e.target.value)}>Email</IonInput>
+                    <IonInput value={password} type="password" 
+                    placeholder="Mot de passe"
+                    onIonChange={(e: any) => setPassword(e.target.value)}> 
+                    Mot de passe</IonInput>
+                    <IonButton color ="primary" onClick={login}>Connection </IonButton>
+                </IonCard>
+                <IonCard>
+                    <IonButton routerLink="/Inscription" color ="primary">Inscription</IonButton>
                 </IonCard>
             </IonContent>
         </IonPage>
--- r2/src/vue/inscription/Inscription.tsx
+++ r2/src/vue/inscription/Inscription.tsx
@@ -3,19 +3,34 @@ import React, { useState } from "react";
 import './Inscription.css';
 
 const Inscription: React.FC = () => {
-    const [input,setInput] = useState<string>('')
+    const [userMail,setUserMail] = useState('')
+    const [password,setPassword] = useState('')
+    const [cfPassword,setcfPassword] = useState('')
+    function login() {
+        console.log(userMail, password)
+    }
+
+
     return (
         <IonPage>
             <IonContent color={"light"}>
                 <IonCard color={"secondary"}>
-                    <IonCardTitle>Connection</IonCardTitle>
-                    <IonInput value={input} type="email">Mail</IonInput> 
-                    <IonInput value={input} type="password">Mot de passe
-                    </IonInput>
-                    <IonCheckbox>
-                            <IonIcon name="eye-outline"></IonIcon>
-                    </IonCheckbox>
-                    <IonButton color ="primary">Connection </IonButton>
+                    <IonCardTitle>Inscription</IonCardTitle>
+                        <IonInput value={userMail} type="email" 
+                        placeholder="VotreEmail" onIonChange={(e: any) => setUserMail(e.target.value)}>
+                        Email </IonInput>
+                        <IonInput value={password} type="password" placeholder="Mot de passe"
+                        onIonChange={(e: any) => setPassword(e.target.value)}> 
+                        Mot de passe</IonInput>
+                        <IonInput value={cfPassword} type="password" 
+                        placeholder="Confirmation"
+                        onIonChange={(e: any) => setcfPassword(e.target.value)}> 
+                        Confirmation de mot de passe</IonInput>     
+                    <IonButton color ="primary" onClick={login}>Inscription </IonButton>
+                </IonCard>
+
+                <IonCard>
+                    <IonButton routerLink="/Connection" color ="primary">Connection</IonButton>
                 </IonCard>
             </IonContent>
         </IonPage>
