00bc4a5ebd7b8aa81367845e5a90fadd23185ab2
AymericBouchetCesi <aymeric.bouchet@viacesi.fr>
2021-06-23 15:43:24 +0200

    Update new-post.spec.js

--- cypress/integration/new-post.spec.js
+++ cypress/integration/new-post.spec.js
@@ -5,6 +5,6 @@ context('New post', () => {
 
     it('New post Navigation success', () => {
         cy.get('[id=new-post]').click();
-        cy.url().should('eq', 'http://localhost:8100/post/newPost')
+        cy.url().should('eq', 'http://localhost:3000/post/newPost')
     });
 });
